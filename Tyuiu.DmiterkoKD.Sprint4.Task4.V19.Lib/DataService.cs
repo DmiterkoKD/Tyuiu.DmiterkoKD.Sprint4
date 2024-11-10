651ad09ad6897e0b4e1726511865f360f0f1864e
DmiterkoKD <kiradmiterko@gmail.com>
2024-11-10 13:41:45 +0500

    Task4

---
+++ Tyuiu.DmiterkoKD.Sprint4.Task4.V19.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.DmiterkoKD.Sprint4.Task4.V19.Lib
+{
+    public class DataService : ISprint4Task4V19
+    {
+        public int Calculate(int[,] matrix)
+        {
+            int rows = matrix.GetUpperBound(0) + 1;
+            int cols = matrix.Length / rows;
+
+            int c = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        c++;
+                    }
+                }
+            }
+            return c;
+        }
+    }
+}
---
+++ Tyuiu.DmiterkoKD.Sprint4.Task4.V19/Program.cs
@@ -0,0 +1,60 @@
+﻿using Tyuiu.DmiterkoKD.Sprint4.Task4.V19.Lib;
+namespace Tyuiu.DmiterkoKD.Sprint4.Task4.V19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнила: Дмитерко К.Д. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы. (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #19                                                             *");
+            Console.WriteLine("* Выполнила: Дмитерко Кира Дмитриевна | ИБКСб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая из целочисленного двумерного массива,       *");
+            Console.WriteLine("* введеного с клавиатуры, подсчитает количество нечетных элементов массива.*");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите количество строк в массиве: ");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов в массиве: ");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            int[,] mtrx = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.WriteLine($"Введите {i}, {j} элемент массива: ");
+                    mtrx[i,j] = Convert.ToInt32(Console.ReadLine());
+                }         
+            }
+
+            Console.WriteLine("\nМассив");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.WriteLine($"{mtrx[i,j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(mtrx);
+            Console.WriteLine("Количество нечетных элементов = " + res);
+        }
+    }
+}
